d50c3884c548591b224696ad4b5f36f4f4ada202
RheaBubbles <augbubbles@foxmail.com>
2017-08-10 16:48:29 +0800

    优化感知传感器数据显示界面（横屏适配）
    Change codes
    
    change file: SensorActivity.java,activity_sensor.xml
    
    传感器数据展示改为横板适应布局
    预警开关设置为Switch以及显示当前状态
    
    add file: res\drawable\bg_for_sensor.xml
    
    添加的xml文件为布局背景文件
    
    Create by yumi
    2017/8/10 16:35

--- app/src/main/java/cn/edu/hit/ftcl/wearablepc/Sensor/SensorActivity.java
+++ app/src/main/java/cn/edu/hit/ftcl/wearablepc/Sensor/SensorActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,25 +28,62 @@ import cn.edu.hit.ftcl.wearablepc.R;
 import cn.edu.hit.ftcl.wearablepc.Sensor.model.HeartRateTable;
 
 public class SensorActivity extends AppCompatActivity {
+    /*
     private TextView last_update_time_tv;
     private Button warnning, cancle;
     int colorPrimary;
     private TextView temperature_tv, alcohol_tv, humidity_tv, pressure_tv;
+    */
+
+    private TextView time_y_m_d,time_h_m,warning_state;
+    private TextView heart_rate,temperature,humidity,pressure,alcohol,pulse;
+    private Switch warning_switch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
-        last_update_time_tv = (TextView)findViewById(R.id.last_update_time);
-        last_update_time_tv.setText(getSystemTime());
+        // 此处 layout : activity_sensor.xml 已更改为横屏适配模式 by Yumi 2017/8/10 16:25
 
-        colorPrimary = last_update_time_tv.getCurrentTextColor();
+        //last_update_time_tv = (TextView)findViewById(R.id.last_update_time);
+        //last_update_time_tv.setText(getSystemTime());
+        time_y_m_d = (TextView)findViewById(R.id.time_y_m_d);
+        time_h_m = (TextView)findViewById(R.id.time_h_m);
+        time_y_m_d.setText(getSystemYMD());
+        time_h_m.setText(getSystemHM());
 
-        temperature_tv = (TextView) findViewById(R.id.temperature_tv);
-        alcohol_tv = (TextView) findViewById(R.id.alcohol_tv);
-        humidity_tv = (TextView) findViewById(R.id.humidity_tv);
-        pressure_tv = (TextView) findViewById(R.id.pressure_tv);
+        //colorPrimary = last_update_time_tv.getCurrentTextColor();
+        //temperature_tv = (TextView) findViewById(R.id.temperature_tv);
+        //alcohol_tv = (TextView) findViewById(R.id.alcohol_tv);
+        //humidity_tv = (TextView) findViewById(R.id.humidity_tv);
+        //pressure_tv = (TextView) findViewById(R.id.pressure_tv);
 
+        heart_rate = (TextView) findViewById(R.id.heartRateNumber);
+        temperature = (TextView) findViewById(R.id.temperatureNumber);
+        humidity = (TextView) findViewById(R.id.humidityNumber);
+        pressure = (TextView) findViewById(R.id.airPressureNumber);
+        alcohol = (TextView) findViewById(R.id.alcoholNumber);
+        pulse = (TextView) findViewById(R.id.pulseNumber);
+        warning_state = (TextView) findViewById(R.id.warning_state);
+        warning_switch = (Switch) findViewById(R.id.warning_switch);
+        // switch开关用于管理预警开启和关闭
+        warning_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    Toast.makeText(getApplicationContext(),"预警功能开启",Toast.LENGTH_SHORT).show();
+                    warning_state.setText("已开启");
+                    Intent intent = new Intent(SensorActivity.this, SensorReceiveService.class);
+                    startService(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),"预警功能关闭",Toast.LENGTH_SHORT).show();
+                    warning_state.setText("已关闭");
+                    Intent stopServiceIntent = new Intent(SensorActivity.this, SensorReceiveService.class);
+                    stopService(stopServiceIntent);
+                }
+            }
+        });
+        /*
         warnning = (Button)findViewById(R.id.button_warnning);
         cancle = (Button) findViewById(R.id.button_warnning_cancle);
         warnning.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +102,9 @@ public class SensorActivity extends AppCompatActivity {
                 stopService(stopServiceIntent);
             }
         });
+        */
 
-        showSensorData();
+        // showSensorData();
     }
 
     /**
@@ -88,10 +128,19 @@ public class SensorActivity extends AppCompatActivity {
         cursor3.moveToLast();
         int concentration = cursor3.getInt(cursor3.getColumnIndex("concentration"));
 
+        //heart_rate.setText(); 暂无心率数据
+        temperature.setText(""+temperatureValue);
+        humidity.setText(""+humidityValue);
+        pressure.setText(""+pressureValue);
+        alcohol.setText(""+concentration);
+        //pulse.setText(); 暂无脉搏数据
+
+        /*
         temperature_tv.setText(""+temperatureValue);
         humidity_tv.setText(""+humidityValue);
         pressure_tv.setText(""+pressureValue);
         alcohol_tv.setText(""+concentration);
+        */
 
         cursor0.close();
         cursor1.close();
@@ -110,8 +159,15 @@ public class SensorActivity extends AppCompatActivity {
     }
 
 
-    public String getSystemTime(){
-        SimpleDateFormat format =new SimpleDateFormat("yyyy-MM-dd  hh:mm:ss");
+    public String getSystemYMD(){
+        SimpleDateFormat format =new SimpleDateFormat("yyyy-MM-dd");
+        Date curdate = new Date(System.currentTimeMillis());
+        String str = format.format(curdate);
+        return str;
+    }
+
+    public String getSystemHM(){
+        SimpleDateFormat format =new SimpleDateFormat("hh:mm");
         Date curdate = new Date(System.currentTimeMillis());
         String str = format.format(curdate);
         return str;
